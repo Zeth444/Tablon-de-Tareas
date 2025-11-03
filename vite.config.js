6fc0d5e6c8456e9763478839bb4c4798b86b991a
Zeth4444 <alejandromuralles56@gmail.com>
2025-11-03 07:23:06 -0600

    Configurar archivos para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Esta es la línea que añadimos para el despliegue
+  base: "/Tablon-de-Tareas/" 
 })
